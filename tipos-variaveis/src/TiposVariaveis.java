1d7253bc5efba4725e43a9bbf9dbeab601163054
Paola Russo <paola93russo@gmail.com>
2023-10-27 15:16:52 -0300

    feat: novas variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
-public class App {
+public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        String meuNome = "Paola Russo";
+
+        
     }
+  
 }
